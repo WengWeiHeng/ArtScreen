8256153cc4b5db7dbb02df789864bb0b94a602a3
PeiWenLiu <y61y873s@gmail.com>
2020-07-17 21:02:19 +0900

    Add new branch By Tam

---
+++ ArtScreen/Controller/TestBranchByTam.swift
@@ -0,0 +1,20 @@
+//
+//  TestBranchByTam.swift
+//  ArtScreen
+//
+//  Created by Tammy37 on 2020/7/17.
+//  Copyright © 2020 Heng. All rights reserved.
+//
+
+import UIKit
+
+class TestBranchByTam: UIViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Hello World!")
+    }
+}
